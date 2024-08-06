fc9d686681d5ec12108bf0ade7714ad989897b75
DanSViana <sousadaniel208@gmail.com>
2024-08-06 17:12:25 -0300

    Versão 1.0

---
+++ main.py
@@ -0,0 +1,22 @@
+# lista 
+nomes = ['Fulano', 'Cicrano', 'Beltrano', 'João', 'Maria', 'José']
+
+# exibe a lista e seus respectivos índices
+for i  in range(len(nomes)):
+    print(f'Índice {i}: {nomes[i]}.')
+
+# quebra de linha
+print('\n')
+
+try:
+    # usuário informa o indice
+    indice = int(input('Informe o número do índice: '))
+
+    # faz a alteração
+    nomes[indice] = input('Informe o novo nome:').capitalize()
+except:
+    print('Não foi possível fazer a alteração')
+
+# exibe a nova lista 
+for i in range(len(nomes)):
+    print(f'Índice {i}: {nomes[i]}.')
